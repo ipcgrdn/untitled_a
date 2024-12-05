21de5b7963ebd0e2f50522c95db73b9477c42703
choi <cejewe@naver.com>
2024-12-06 02:15:37 +0900

    모바일 최적화 1차

--- src/components/container/rectangle-container.tsx
+++ src/components/container/rectangle-container.tsx
@@ -19,7 +19,8 @@ const RectangleContainer = ({
   duration,
 }: RectangleContainerProps) => {
   return (
-    <div className="flex text-black items-center gap-x-4 p-2 mb-2 rounded-lg hover:bg-white/30 transition-colors">
+    <div className="flex text-black w-full items-center gap-x-4 p-2 mb-2 rounded-lg hover:bg-white/30 transition-colors">
+      <div className="flex">
       <Image
         src={cover}
         alt="cover"
@@ -27,13 +28,14 @@ const RectangleContainer = ({
         height={44}
         className="rounded-lg"
       />
+      </div>
       <div className="flex-1 min-w-0">
         <h3 className="font-semibold text-lg truncate">{name}</h3>
         <p className="text-sm text-neutral-700 truncate">
           {artist} · {album}
         </p>
       </div>
-      <div className="flex items-center gap-x-2">
+      <div className="flex items-center gap-x-2 mx-auto">
         <span className="text-base whitespace-nowrap">{duration}</span>
         <button className="cursor-pointer">
           <IconDotsVertical className="size-5" />
